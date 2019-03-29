7402e0b5473cce4c761cb310a3232bf9afcee4cb
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-28 22:44:51 -0400

    Completes index.js file to produce a functioning server at port 4000

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+    console.log(`\n***Web-DB-IV-Challenge API running on http://localhost${port}***\n`);
+});
